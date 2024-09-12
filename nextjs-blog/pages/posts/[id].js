007c422878eb2990665c8000dfa9e4bcb49e8251
minnggy23 <rubyshemaria@gmail.com>
2024-09-12 11:53:50 -0700

    Final

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,6 +1,10 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
+// Add this import
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
+
 export async function getStaticProps({ params }) {
   // Add the "await" keyword like this:
   const postData = await getPostData(params.id);
@@ -16,16 +20,15 @@ export default function Post({ postData }) {
   return (
     <Layout>
       <Head>
-      <title>{postData.title}</title>
-
+        <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
